3208e54dd5e8e2bd98b99da3fe5894f263d68fc8
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-05-31 22:25:15 +0530

    user route created

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,9 +1,32 @@
 import { connectToDB } from "@mongodb";
-
+import User from "@models/user";
+import { hash } from "bcryptjs";
 export const POST = async (req, res) => {
   try {
     await connectToDB();
     const body = await req.json();
+
+    const { username, email, password } = body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return new Response("User already exists", {
+        status: 400,
+      });
+    }
+
+    const hashedPassword = await hash(password, 15);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return new Response(JSON.stringify(newUser), { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response("Failed to create a new user", {
