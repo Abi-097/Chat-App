1aed881997d88c7553ffc5161a309f5eead9f33f
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-06-04 21:17:24 +0530

    chat created

--- app/(root)/chats/page.jsx
+++ app/(root)/chats/page.jsx
@@ -1,14 +1,14 @@
-// import ChatList from "@components/ChatList";
-// import Contacts from "@components/Contacts";
+import ChatList from "@components/ChatList";
+import Contacts from "@components/Contacts";
 
 const Chats = () => {
   return (
     <div className="main-container">
       <div className="w-1/3 max-lg:w-1/2 max-md:w-full">
-        ss {/* <ChatList /> */}
+        <ChatList />
       </div>
       <div className="w-2/3 max-lg:w-1/2 max-md:hidden">
-        {/* <Contacts /> */}
+        <Contacts />
       </div>
     </div>
   );
