77504a4cd70a3b69d78e06c4e052b4b7e2bdda17
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-06-05 20:06:20 +0530

    contacts added

--- components/ChatList.jsx
+++ components/ChatList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const ChatList = () => {
-  return <div className="chat-list"></div>;
+  return (
+    <div className="chat-list">
+      <input
+        placeholder="Search chat"
+        className="input-search"
+        // value={search}
+        // onChange={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default ChatList;
